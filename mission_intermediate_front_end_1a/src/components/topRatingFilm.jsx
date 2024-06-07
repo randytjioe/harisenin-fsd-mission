c350a60abb08d15922caee0d03943cf2f662e0a3
randytjioe <randytjioe95@gmail.com>
2024-06-07 21:26:51 +0700

    add pop-over

--- mission_intermediate_front_end_1a/src/components/topRatingFilm.jsx
+++ mission_intermediate_front_end_1a/src/components/topRatingFilm.jsx
@@ -1,29 +1,39 @@
-import { IoMdArrowRoundForward } from "react-icons/io";
+import { BsFillPlayCircleFill } from "react-icons/bs";
+import { FaRegCircleCheck } from "react-icons/fa6";
+import {
+  IoIosArrowDropdownCircle,
+  IoMdArrowRoundForward,
+} from "react-icons/io";
 import { IoArrowBackSharp } from "react-icons/io5";
 
 export default function TopRatingFilm() {
   const images = [
     {
-      name: "Dont't Look Up",
+      id: 1,
       src: "/assets/top_1.jpg",
       new: true,
+      popOver: "/assets/top_p_1.png",
     },
     {
-      name: "Dont't Look Up",
+      id: 2,
       src: "/assets/top_2.jpg",
+      popOver: "/assets/top_p_2.png",
     },
     {
-      name: "Dont't Look Up",
+      id: 3,
       src: "/assets/top_3.jpg",
+      popOver: "/assets/top_p_3.png",
     },
     {
-      name: "Dont't Look Up",
+      id: 4,
       src: "/assets/top_4.jpg",
       new: true,
+      popOver: "/assets/top_p_4.png",
     },
     {
-      name: "Dont't Look Up",
+      id: 5,
       src: "/assets/top_5.jpg",
+      popOver: "/assets/top_p_5.png",
     },
   ];
   return (
@@ -37,20 +47,55 @@ export default function TopRatingFilm() {
         </div>
         {images.map((images) => {
           return (
-            <div
-              className={`relative w-full max-w-sm h-[365px] w-[234px]  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 bg-cover`}
-              style={{ backgroundImage: `url(${images.src})` }}
-            >
-              <div className="pb-5 text-white ">
-                <div className="absolute flex items-center justify-between  px-5 top-3 left-2 bg-[#0F1E93] rounded-[24px]">
-                  <span className="text-[18px] font-bold  dark:text-white">
-                    {images.new === true ? "Episode Baru" : ""}
-                  </span>
+            <>
+              <div
+                className={`relative w-full max-w-sm h-[365px] w-[234px]  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 bg-cover`}
+                style={{ backgroundImage: `url(${images.src})` }}
+                data-popover-target={`popover-default-${images.id}`}
+                data-popover-placement="bottom-end"
+              >
+                <div className="pb-5 text-white ">
+                  <div className="absolute flex items-center justify-between  px-5 top-3 left-2 bg-[#0F1E93] rounded-[24px]">
+                    <span className="text-[18px] font-bold  dark:text-white">
+                      {images.new === true ? "Episode Baru" : ""}
+                    </span>
+                  </div>
                 </div>
               </div>
-            </div>
+              <div
+                data-popover=""
+                id={`popover-default-${images.id}`}
+                role="tooltip"
+                className="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-[#181A1C] border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800"
+              >
+                <img src={images.popOver} />
+
+                <div className="px-3 py-2">
+                  <div className="flex flex-col">
+                    <div className="flex justify-between">
+                      <div className="flex gap-5">
+                        <BsFillPlayCircleFill className="text-white h-7 w-7" />
+                        <FaRegCircleCheck className="text-white h-7 w-7" />
+                      </div>
+
+                      <div>
+                        <IoIosArrowDropdownCircle className="text-white h-7 w-7" />
+                      </div>
+                    </div>
+                    <div className="flex gap-3 pt-3 text-white">
+                      <div className="bg-[#C1C2C4] rounded-[24px] px-2">
+                        13 +
+                      </div>
+                      <div> 2j 33m</div>
+                    </div>
+                  </div>
+                </div>
+                <div data-popper-arrow="" />
+              </div>
+            </>
           );
         })}
+
         <div className="flex items-center justify-center p-2 ">
           <IoMdArrowRoundForward className="bg-[#2F3334]  rounded-full w-[25px] h-[25px]" />
         </div>
